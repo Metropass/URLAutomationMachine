03d12b6a0eff92af0db429e45f45f9a09955e8a8
Mo <58116522+Metropass@users.noreply.github.com>
2020-09-28 21:03:35 -0400

    Added Threading

--- urlChecker.py
+++ urlChecker.py
@@ -1,45 +1,45 @@
 import argparse
-import re
-from colorama import Fore
-import urllib3
-import sys
-
+import threading
+from urlClass import urlAutomationMachine
 
 # Main function parses each line looking for urls to check and make requests
-# to check whether the url is broken or not
+# to check whether the url is broken or not. Calls the appropriate check function
+# based on the argument passed
 # Param: inputFile
-# Return: Void
+# Return: void
 
 
-def main(fileInput):
-    try:
-        fileToCheck = open(fileInput, 'r')
-        linesInFile = fileToCheck.readlines()
-        http = urllib3.PoolManager()
-        for line in linesInFile:
-            line = line.strip()
-            urlLines = re.match(r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', line)
-            r = http.request('GET', line)
-            if (r.status == 200):
-                print(Fore.GREEN + f"{urlLines} passes automation. This url is working properly!" )
-            elif (r.status == 403):
-                print(Fore.BLACK + f"{urlLines} gives off a warning. This url is fishy!")
-            else:
-                print(Fore.RED + f"{urlLines} fails automation. This url is broken unfortunately!")
-    except OSError:
-        print("Cannot Open The File! Make sure it's readable.")
-    except:
-        print("Unexpected Error: ", sys.exc_info()[0])
-if __name__ == "__main__":
-    parse = argparse.ArgumentParser(description="Checks the file input for any broken HTML urls")
-    parse.add_argument('-f', help="The input file to check")
-    parse.add_argument('-v', action="store_true", help="Displays current version of program")
-    args = parse.parse_args()
-    if (args.v):
-        print("UrlAtuomationMachine ver 1.0")
-    elif (args.f):
-        main(args.f)
+def checkURL(fileInput):
+    input = urlAutomationMachine(fileInput)
+    input.processUrl()
+
+
+def checkFile(fileInput):
+   input = urlAutomationMachine(fileInput)
+   input.processFile()
+
+
+def main(args):
+    if (args.f):
+      threading.Thread(target=checkFile(args.f)).start()
+    elif (args.u):
+      threading.Thread(target=checkURL(args.u)).start()
+    elif (args.v):
+      print("URLAutomationMachine Ver 1.1")
     else:
-        print("This program has two arguments, one for inputting the file, the second one displays the current version of the program")
-        print("Usage: urlChecker [-f] inputFile: The input file to be processed")
-        print("Usage: urlChecker [-v]: Displays current version of the program")
+      print("This program has two arguments, one for inputting the file, the second one displays the current version of the program")
+      print("Usage: urlChecker [-f] inputFile: The input file to be processed")
+      print("Usage: urlChecker [-v]: Displays current version of the program")
+      print("Usage: urlChecker [-u] inputUrl: Checks URL to see if it works or not")
+
+
+if __name__ == "__main__":
+
+   parse = argparse.ArgumentParser(description="Checks the file input for any broken HTML urls")
+   parse.add_argument('-f', help="The input file to check")
+   parse.add_argument('-v', action="store_true", help="Displays current version of program")
+   parse.add_argument('-u', help="The URL to check")
+
+   args = parse.parse_args()
+
+   main(args)
